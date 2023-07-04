7a1fa5a0a99157e0d6c959d80b6bac1ac5fd8591
John Lucas <joohnzay@gmail.com>
2023-07-04 10:00:21 -0300

    nav

--- frontend/src/views/Screen.vue
+++ frontend/src/views/Screen.vue
@@ -1,3 +1,32 @@
 <template>
-    <h1>Inicio</h1>
+    <!--Nav-->
+    <div class="header">
+        <nav>
+            <div class="logo">
+                <img src="../assets/imagem/image_logo.png" alt="logo"> <!--ADICIONAR LOGO-->
+                <img src="../assets/imagem/logonome.png" alt="logonome">
+            </div>
+            <ul id="navbar">
+                <li><a href="#um">Home</a></li>
+                <li><a href="#dois">Serviços</a></li>
+                <li><a href="#tres">Carousel</a></li>
+            </ul>
+        </nav>
+        <!--Main-->
+        <div class="main">
+            <h1>Chegou a hora de levar seu petshop para o próximo nível<br> com nosso sistema de gestão moderno e
+                eficiente.<br>
+            </h1>
+            <a href="#">Faça um teste</a>
+        </div>
+        <div class="right-box">
+            <img src="../assets/imagem/image-rightbox.png" alt="">
+        </div>
+        <svg style="margin-bottom: -1;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" id="dois">
+            <path fill="#04294d" fill-opacity="1"
+                d="M0,32L48,53.3C96,75,192,117,288,144C384,171,480,181,576,186.7C672,192,768,192,864,213.3C960,235,1056,277,1152,282.7C1248,288,1344,256,1392,240L1440,224L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z">
+            </path>
+        </svg>
+    </div>
 </template>
+
